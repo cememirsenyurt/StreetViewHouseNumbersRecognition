91601b34d026b119bd690f082b4c581b9151c935
sampoulin <spoulin1234@gmail.com>
2023-03-11 18:37:31 -0800

    Added loadDataFromPath function to helpers.py

--- helpers.py
+++ helpers.py
@@ -19,6 +19,15 @@ def loadData(dataset: str) -> "tuple[np.ndarray, np.ndarray]":
     mat_path = os.path.join(os.path.curdir, "datasets", mat_name)
     mat_contents = sio.loadmat(mat_path)
     return mat_contents['X'], mat_contents['y']
+
+def loadDataFromPath(p: str) -> "tuple[np.ndarray, np.ndarray]":
+    '''
+    Loads and returns labelled SVHN data from the dataset located at p,
+    a path relative to the current directory.
+    '''
+    mat_path = os.path.join(os.path.curdir, "p")
+    mat_contents = sio.loadmat(mat_path)
+    return mat_contents['X'], mat_contents['y']
     
 
 if __name__ == "__main__":
