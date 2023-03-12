26c47d49244517c1b395e3a65d4799a8a23c47d6
sampoulin <spoulin1234@gmail.com>
2023-03-11 20:26:56 -0800

    Created CNNTest Notebook, updated loadData and loadDataFromPath

--- helpers.py
+++ helpers.py
@@ -18,16 +18,34 @@ def loadData(dataset: str) -> "tuple[np.ndarray, np.ndarray]":
     mat_name = mat_names[dataset]
     mat_path = os.path.join(os.path.curdir, "datasets", mat_name)
     mat_contents = sio.loadmat(mat_path)
-    return mat_contents['X'], mat_contents['y']
+    X, y = mat_contents['X'], mat_contents['y']
+    #convert X to [0,1]
+    X = X/255
+    #swap index axis to first axis
+    X = np.swapaxes(X,3,2)
+    X = np.swapaxes(X,2,1)
+    X = np.swapaxes(X,1,0)
+    #turn 10s into 0s for simplicity
+    y = np.where(y == 10, 0, y)
+    return X, y
 
-def loadDataFromPath(p: str) -> "tuple[np.ndarray, np.ndarray]":
+def loadDataFromPath(relative_path: str) -> "tuple[np.ndarray, np.ndarray]":
     '''
     Loads and returns labelled SVHN data from the dataset located at p,
     a path relative to the current directory.
     '''
-    mat_path = os.path.join(os.path.curdir, "p")
+    mat_path = os.path.join(os.path.curdir, relative_path)
     mat_contents = sio.loadmat(mat_path)
-    return mat_contents['X'], mat_contents['y']
+    X, y = mat_contents['X'], mat_contents['y']
+    #convert X to [0,1]
+    X = X/255
+    #swap index axis to first axis
+    X = np.swapaxes(X,3,2)
+    X = np.swapaxes(X,2,1)
+    X = np.swapaxes(X,1,0)
+    #turn 10s into 0s for simplicity
+    y = np.where(y == 10, 0, y)
+    return X, y
     
 
 if __name__ == "__main__":
